532f42bee0d2d92c0c88ffda93d16ab7330e1d64
Accountingsocietyhc <accountingsocietyhc@gmail.com>
2026-09-08 14:04:40 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://hunteraccountingsociety.me',
+  site: 'hunteraccountingsociety.me',
   vite: {
     plugins: [tailwindcss()]
   }
