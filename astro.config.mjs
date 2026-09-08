655fe66fab5fa1b0daf2ecfeea595a667dae87d3
Accountingsocietyhc <accountingsocietyhc@gmail.com>
2026-09-08 13:45:35 -0400

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ashctest.me',
+  site: 'http://hunteraccountingsociety.me',
   vite: {
     plugins: [tailwindcss()]
   }
